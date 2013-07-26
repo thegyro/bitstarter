25bffe102a18df180bb711bdf9f4ab4969299719
Srinath R <srinath132@gmail.com>
2013-07-27 00:25:45 +0530

    Completed final version of grader.json , updated checks.json and added output.json

--- grader.js
+++ grader.js
@@ -7,6 +7,14 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('restler');
+
+var getResp = function(url){
+  rest.get(url).on('complete', function(response){
+    return response.toString();
+  });
+};
+
 
 var assertFileExists = function(infile){
     var instr = infile.toString();
@@ -19,23 +27,32 @@ var assertFileExists = function(infile){
 };
 
 
-var cheerioHtmlFile = function(htmlfile){
-    return cheerio.load(fs.readFileSync(htmlfile));
+var cheerioHtmlFile = function(htmlfile,htmlstring){
+    if(htmlstring == undefined )
+        return cheerio.load(fs.readFileSync(htmlfile));
+    else
+        return cheerio.load(htmlstring);
 };
 
 var loadChecks = function(checksfile){
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile,checksfile){
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtmlFile = function(htmlfile,checksfile,url){
+    if(url== undefined){      
+        $ = cheerioHtmlFile(htmlfile);
+    }
+    else{
+        $ = cheerioHtmlFile(htmlfile,getResp(url));
+    }
     var checks = loadChecks(checksfile).sort();
 
-    var out = {};
-    for(var ii in checks){
-	var present = $(checks[ii]).length > 0;
-	out[checks[ii]] = present;
-    }
+        var out = {};
+        for(var ii in checks){
+	       var present = $(checks[ii]).length > 0;
+	       out[checks[ii]] = present;
+        }
+    
 
     return out;
 };
@@ -51,11 +68,14 @@ if ( require.main == module){
     program
         .option('-c,--checks <check_file>','Path to checks.json',clone(assertFileExists),CHECKSFILE_DEFAULT)
         .option('-f,--file <html_file>','Path to index.html',clone(assertFileExists),HTMLFILE_DEFAULT)
-        .parse(process.argv);
-
-    var checkJson = checkHtmlFile(program.file,program.checks);
+        .option('-u,--url <url>','url of required file')
+        .parse(process.argv);    
+    
+    console.log(program.url);
+    var checkJson = checkHtmlFile(program.file,program.checks,program.url);
     var outJson = JSON.stringify(checkJson,null,4);
     console.log(outJson);
+    fs.writeFileSync("output.json",outJson);
 }
 else{
     exports.checkHtmlFile = checkHtmlFile;
